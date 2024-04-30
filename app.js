8a27c772d2936a8c85087f12ba0fbb429fc5d53f
Ashar Khan <asharullah.khan2468@gmail.com>
2024-04-30 17:33:14 +0500

    strings methods files

---
+++ app.js
@@ -0,0 +1,56 @@
+//Question no 1
+
+// let paragraph=document.querySelector(".para1")
+
+// let firstName = prompt("Enter your First Name");
+// let lastName = prompt("Enter your Last Name");
+
+// let fullName = ( firstName  +  lastName);
+
+// paragraph.innerHTML=`Hello ! ${ fullName}`
+
+
+
+//Question no 2
+
+//  let paragraph2=document.querySelector(".para2");
+
+// let userMobile = prompt("Enter your favourite mobile");
+
+
+// paragraph2.innerHTML = `My favourite phone is : ${userMobile}<br>
+// Lenght of string: ${userMobile.length}`;
+
+
+
+//Question no 3
+
+// let word="Pakistani";
+
+// document.write(word.indexOf("n"))
+
+
+
+//Question no 4
+
+// let greet="Hello world"
+// document.write(`String: ${greet}<br>
+// last index of 'l': ${greet.lastIndexOf("l")}`);
+
+
+
+//Question no 5
+
+
+// let country="Pakistani";
+
+// document.write(`String: ${country}<br>
+// Character at index 3: ${country.charAt(3)}`)
+
+
+
+//Question no 6
+
+
+
+
